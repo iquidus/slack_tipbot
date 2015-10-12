31e2a5b9d9c6fd21264c7d0dc8858d2efec1e618
sigwo <steven@sigwo.com>
2015-10-12 13:52:18 -0400

    add influxcoin

---
+++ coin_config/influxcoin.rb
@@ -0,0 +1,23 @@
+module Influxcoin
+  PERFORM_ERROR = 'That error doesn\'t exist'
+  BALANCE_REPLY_PRETEXT = 'Your balance: '
+  CURRENCY_ICON = 'INFX'
+  WEALTHY_UPPER_BOUND = 100
+  WEALTHY_UPPER_BOUND_POSTTEXT = ' very wealthy!'
+  WEALTHY_UPPER_BOUND_EMOJI = ':moneybag:'
+  BALANCE_REPLY_POSTTEXT = ' '
+  DEPOSIT_PRETEXT = 'Make a deposit'
+  DEPOSIT_POSTTEXT = 'this is your address'
+  TIP_ERROR_TEXT = 'pls say tip @username amount'
+  TIP_PRETEXT = 'So generous!'
+  TIP_POSTTEXT1 = 'http://explorer.influxcoin.xyz/tx/'
+  TIP_POSTTEXT2 = '|blockchain'
+  WITHDRAW_TEXT = 'What the Flux'
+  WITHDRAW_ICON = ':shit:'
+  NETWORKINFO_ICON = ':bar_chart:'
+  TOO_POOR_TEXT = 'Too poor. Sorry.'
+  NO_PURPOSE_LOWER_BOUND_TEXT = 'Too small. No purpose'
+  NO_PURPOSE_LOWER_BOUND = 0.00000001
+  RANDOMIZED_EMOJI = ':black_joker:'
+  NETWORK = 'influxcoin'
+end
