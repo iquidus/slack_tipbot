da281070dd0e08792293b4ef88d79de62d34c292
Steven <steven@sigwo.com>
2016-11-19 23:22:02 +0000

    Stratis add

---
+++ coin_config/stratis.rb
@@ -0,0 +1,23 @@
+module Stratis
+  PERFORM_ERROR = 'That error doesn\'t exist'
+  BALANCE_REPLY_PRETEXT = 'Your balance: '
+  CURRENCY_ICON = ' Stratis'
+  WEALTHY_UPPER_BOUND = 100
+  WEALTHY_UPPER_BOUND_POSTTEXT = ' very wealthy!'
+  WEALTHY_UPPER_BOUND_EMOJI = ':moneybag:'
+  BALANCE_REPLY_POSTTEXT = ' '
+  DEPOSIT_PRETEXT = 'Make a deposit'
+  DEPOSIT_POSTTEXT = 'this is your address'
+  TIP_ERROR_TEXT = 'pls say tip @username amount'
+  TIP_PRETEXT = ''
+  TIP_POSTTEXT1 = 'https://chainz.cryptoid.info/strat/tx.dws?'
+  TIP_POSTTEXT2 = '.htm'
+  WITHDRAW_TEXT = 'Leaving so soon?'
+  WITHDRAW_ICON = ':shit:'
+  NETWORKINFO_ICON = ':bar_chart:'
+  TOO_POOR_TEXT = 'Too poor. Do not forget the tx fee (0.1)'
+  NO_PURPOSE_LOWER_BOUND_TEXT = 'Too small. No purpose'
+  NO_PURPOSE_LOWER_BOUND = 0.00000001
+  RANDOMIZED_EMOJI = ':black_joker:'
+  NETWORK = 'stratis'
+end
